8597b9677c621b30907aab03355777348518d0d6
Gonçalo Palaio <goncalo.palaio@gmail.com>
2012-08-30 15:10:14 +0100

    added buffer utilities

---
+++ Android/common/BufferUtils.java
@@ -0,0 +1,19 @@
+package palisade.particles;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+import java.nio.ShortBuffer;
+
+public class BufferUtils {
+	public static FloatBuffer createFloatBuffer(int size) {
+        ByteBuffer bb = ByteBuffer.allocateDirect(size * 4);
+        bb.order(ByteOrder.nativeOrder());
+        return bb.asFloatBuffer();
+	}
+	public static ShortBuffer createShortBuffer(int size) {
+        ByteBuffer bb = ByteBuffer.allocateDirect(size * 2);
+        bb.order(ByteOrder.nativeOrder());
+        return bb.asShortBuffer();
+	}
+}
